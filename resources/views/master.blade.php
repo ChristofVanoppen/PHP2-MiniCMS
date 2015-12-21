fbfc2ffcbba000f40339e0a524d7c93b13e7be36
unknown <vanoppen.christof@hotmail.com>
2015-12-21 18:18:46 +0100

    register/login added

---
+++ resources/views/master.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mini CMS</title>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
